4cd90f6b25d9801d8bc21d9d61376c505693bd61
DonCastillo <don.qcastillo@yahoo.com>
2021-01-03 01:18:27 -0700

    added event handlers

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    // dont do this !
+    //this.state.persons[0].name = "Maximillian";
+    this.setState({
+      persons: [
+        { name: 'Maximillian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));

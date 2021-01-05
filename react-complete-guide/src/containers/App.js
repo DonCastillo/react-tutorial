dbe11f4e86509a80519b3b4f50af383e754e20e1
DonCastillo <don.qcastillo@yahoo.com>
2021-01-05 00:56:07 -0700

    added stuff in the Persons.js

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 
 
@@ -66,18 +66,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
           <div>
-            {
-              this.state.persons.map((person, index) => {
-                return (
-                  <Person 
-                    click={() => this.deletePersonHandler(index)}
-                    name={person.name} 
-                    age={person.age}
-                    key={person.id}
-                    changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                )   
-              })
-            }
+            <Persons persons={this.state.persons}
+                     clicked={this.deletePersonHandler}
+                     changed={this.nameChangedHandler}/>
           </div>
       );
       
